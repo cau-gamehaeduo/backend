4387a9d4a2e01d81279d0c3bea2e1f950dae3456
Cyma-s <ssol0319@naver.com>
2022-11-03 23:45:29 +0900

    add: initial commit

---
+++ src/main/java/com/cau/gamehaeduo/GamehaeduoApplication.java
@@ -0,0 +1,13 @@
+package com.cau.gamehaeduo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamehaeduoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamehaeduoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cau/gamehaeduo/GamehaeduoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cau.gamehaeduo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamehaeduoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
