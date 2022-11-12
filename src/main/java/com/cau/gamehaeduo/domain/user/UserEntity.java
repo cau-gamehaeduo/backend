fe4e16e9ea51747c3927924cd9fd796723f1c608
Cyma-s <ssol0319@naver.com>
2022-11-13 01:40:39 +0900

    feat : 생성자 추가, FK 어노테이션 수정

--- src/main/java/com/cau/gamehaeduo/domain/user/UserEntity.java
+++ src/main/java/com/cau/gamehaeduo/domain/user/UserEntity.java
@@ -1,7 +1,6 @@
 package com.cau.gamehaeduo.domain.user;
 
 import com.cau.gamehaeduo.domain.player.PlayerEntity;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -28,13 +27,13 @@ public class UserEntity {
     private int userIdx;
     @Column
     private String nickname;
-    @Column
+    @Column(name="profile_photo_url")
     private String profilePhotoUrl;
-    @Column
+    @Column(name="is_player")
     private String isPlayer;
     @Column
     private float rating;
-    @Column
+    @Column(name="created_at")
     private String createdAt;
     @Column
     private String status;
@@ -52,9 +51,27 @@ public class UserEntity {
     @Column(name = "kakao_id")
     private long kakaoIdx;
 
-    @OneToOne(cascade = CascadeType.ALL)
+    @OneToOne
     @PrimaryKeyJoinColumn
     private PlayerEntity player;
+
+    public UserEntity(int userIdx, String nickname, String profilePhotoUrl, String isPlayer, float rating,
+                      String createdAt,
+                      String status, int top, int jungle, int mid, int ad, int supporter, long kakaoIdx) {
+        this.userIdx = userIdx;
+        this.nickname = nickname;
+        this.profilePhotoUrl = profilePhotoUrl;
+        this.isPlayer = isPlayer;
+        this.rating = rating;
+        this.createdAt = createdAt;
+        this.status = status;
+        this.top = top;
+        this.jungle = jungle;
+        this.mid = mid;
+        this.ad = ad;
+        this.supporter = supporter;
+        this.kakaoIdx = kakaoIdx;
+    }
 
     public UserEntity(String nickname, String profilePhotoUrl, int top, int jungle, int mid, int ad, int supporter, long kakaoIdx) {
         this.nickname = nickname;
