07a763faad097272616a586570a43a307a757006
Cyma-s <ssol0319@naver.com>
2022-11-13 00:09:38 +0900

    feat : PlayerResponseDTO 추가

---
+++ src/main/java/com/cau/gamehaeduo/domain/player/PlayerResponseDTO.java
@@ -0,0 +1,15 @@
+package com.cau.gamehaeduo.domain.player;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlayerResponseDTO {
+    private boolean isCreated;
+    private String resultMessage;
+}
