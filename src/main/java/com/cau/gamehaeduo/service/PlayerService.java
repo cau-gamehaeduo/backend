fa0bcefb52a5f0ee5973198c8f1d34c4da083bb2
Cyma-s <ssol0319@naver.com>
2022-11-13 00:20:07 +0900

    feat : Player 등록 함수가 DTO 리턴하도록 변경

--- src/main/java/com/cau/gamehaeduo/service/PlayerService.java
+++ src/main/java/com/cau/gamehaeduo/service/PlayerService.java
@@ -2,17 +2,25 @@ package com.cau.gamehaeduo.service;
 
 import com.cau.gamehaeduo.domain.player.PlayerEntity;
 import com.cau.gamehaeduo.domain.player.PlayerRequestDTO;
+import com.cau.gamehaeduo.domain.player.PlayerResponseDTO;
 import com.cau.gamehaeduo.repository.PlayerRepository;
+import com.cau.gamehaeduo.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class PlayerService {
     private final PlayerRepository playerRepository;
+    private final UserRepository userRepository;
 
-    public void registerPlayer(PlayerRequestDTO requestDto) {
+    @Transactional
+    public PlayerResponseDTO registerPlayer(PlayerRequestDTO requestDto){
+        userRepository.registerPlayer(requestDto.getUserIndex());
         PlayerEntity playerEntity = PlayerRequestDTO.of(requestDto);
         playerRepository.save(playerEntity);
+        return new PlayerResponseDTO(true,
+                "플레이어가 등록되었습니다.");
     }
 }
