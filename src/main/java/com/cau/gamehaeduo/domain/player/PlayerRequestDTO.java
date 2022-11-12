3379369a8840fbea0a0c66b17b538fddcc92044b
Cyma-s <ssol0319@naver.com>
2022-11-13 01:40:07 +0900

    fix : PlayerEntity builder에 id, user 추가

--- src/main/java/com/cau/gamehaeduo/domain/player/PlayerRequestDTO.java
+++ src/main/java/com/cau/gamehaeduo/domain/player/PlayerRequestDTO.java
@@ -1,5 +1,6 @@
 package com.cau.gamehaeduo.domain.player;
 
+import com.cau.gamehaeduo.domain.user.UserEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,22 +9,23 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PlayerRequestDTO {
-    private int userIndex;
+    private int userIdx;
     private int gender;
     private String introduction;
     private String playStyle;
     private String tier;
     private int price;
 
-    public static PlayerEntity of(PlayerRequestDTO requestDto) {
+    public static PlayerEntity of(PlayerRequestDTO requestDto, UserEntity user) {
         return PlayerEntity.builder()
-                .id(requestDto.userIndex)  // user index
+                .id(requestDto.userIdx)  // user index
                 .status("A")
                 .gender(requestDto.gender)
                 .price(requestDto.price)
                 .introduction(requestDto.introduction)
                 .playStyle(requestDto.playStyle)
                 .tier(requestDto.tier)
+                .user(user)
                 .build();
     }
 }
