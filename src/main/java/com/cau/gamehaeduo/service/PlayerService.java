41a71cd5f1b45d92b7775d0327d27b83b7191d58
Cyma-s <ssol0319@naver.com>
2022-11-13 01:41:26 +0900

    feat : 입력 받은 userIdx로 user 찾는 로직 추가

--- src/main/java/com/cau/gamehaeduo/service/PlayerService.java
+++ src/main/java/com/cau/gamehaeduo/service/PlayerService.java
@@ -3,12 +3,16 @@ package com.cau.gamehaeduo.service;
 import com.cau.gamehaeduo.domain.player.PlayerEntity;
 import com.cau.gamehaeduo.domain.player.PlayerRequestDTO;
 import com.cau.gamehaeduo.domain.player.PlayerResponseDTO;
+import com.cau.gamehaeduo.domain.user.UserEntity;
 import com.cau.gamehaeduo.repository.PlayerRepository;
 import com.cau.gamehaeduo.repository.UserRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Log4j2
 @Service
 @RequiredArgsConstructor
 public class PlayerService {
@@ -17,8 +21,10 @@ public class PlayerService {
 
     @Transactional
     public PlayerResponseDTO registerPlayer(PlayerRequestDTO requestDto){
-        userRepository.registerPlayer(requestDto.getUserIndex());
-        PlayerEntity playerEntity = PlayerRequestDTO.of(requestDto);
+        userRepository.registerPlayer(requestDto.getUserIdx());
+        List<UserEntity> userList = userRepository.selectByUserId(requestDto.getUserIdx());
+        UserEntity user = userList.get(0);
+        PlayerEntity playerEntity = PlayerRequestDTO.of(requestDto, user);
         playerRepository.save(playerEntity);
         return new PlayerResponseDTO(true,
                 "플레이어가 등록되었습니다.");
