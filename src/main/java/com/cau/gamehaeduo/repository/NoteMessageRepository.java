589e6b635e786d0d6539cbe674d0da85771c232f
Cyma-s <ssol0319@naver.com>
2022-11-23 01:41:40 +0900

    feat : Repository에 대화방 id로 대화 내용 불러오는 함수 추가

--- src/main/java/com/cau/gamehaeduo/repository/NoteMessageRepository.java
+++ src/main/java/com/cau/gamehaeduo/repository/NoteMessageRepository.java
@@ -1,11 +1,12 @@
 package com.cau.gamehaeduo.repository;
 
 import com.cau.gamehaeduo.domain.note.entity.NoteMessageEntity;
-import com.cau.gamehaeduo.domain.player.PlayerEntity;
+import com.cau.gamehaeduo.domain.note.entity.NoteRoomEntity;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NoteMessageRepository extends JpaRepository<NoteMessageEntity, Long> {
-
+    List<NoteMessageEntity> findByNoteRoom(NoteRoomEntity noteRoom);
 }
