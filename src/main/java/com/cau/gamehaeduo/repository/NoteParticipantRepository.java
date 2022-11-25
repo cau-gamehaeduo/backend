e0095b8ed444b9a65e47f4e9808e45682f45c41c
Cyma-s <ssol0319@naver.com>
2022-11-25 16:42:36 +0900

    feat : 대화방 id로 대화방 참가자들 찾기

--- src/main/java/com/cau/gamehaeduo/repository/NoteParticipantRepository.java
+++ src/main/java/com/cau/gamehaeduo/repository/NoteParticipantRepository.java
@@ -2,6 +2,7 @@ package com.cau.gamehaeduo.repository;
 
 
 import com.cau.gamehaeduo.domain.note.entity.NoteParticipantEntity;
+import com.cau.gamehaeduo.domain.note.entity.NoteRoomEntity;
 import com.cau.gamehaeduo.domain.user.UserEntity;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +11,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface NoteParticipantRepository extends JpaRepository<NoteParticipantEntity, Long> {
     List<NoteParticipantEntity> findByNoteParticipantId(UserEntity user);
+    List<NoteParticipantEntity> findByNoteRoom(NoteRoomEntity noteRoom);
 }
