4d5cf60dcb25709b793971c61f012d41d5554667
Cyma-s <ssol0319@naver.com>
2022-11-25 16:38:51 +0900

    feat : 대화방 리스트 Response DTO 추가

---
+++ src/main/java/com/cau/gamehaeduo/domain/note/MessageRoomsResponseDTO.java
@@ -0,0 +1,22 @@
+package com.cau.gamehaeduo.domain.note;
+
+import java.sql.Timestamp;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Getter
+@Service
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MessageRoomsResponseDTO {
+    private Long roomId;
+    private int duoId;
+    private String duoName;
+    private String profileImageUrl;
+    private String currentMessage;
+    private Timestamp currentMessageTime;
+}
