70ebadbdac7be0f294fd738f0fc5c89b0e2c9dde
Cyma-s <ssol0319@naver.com>
2022-12-01 00:24:27 +0900

    feat : ReviewEntity 생성

---
+++ src/main/java/com/cau/gamehaeduo/domain/review/ReviewEntity.java
@@ -0,0 +1,50 @@
+package com.cau.gamehaeduo.domain.review;
+
+import com.cau.gamehaeduo.domain.user.UserEntity;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+@DynamicInsert
+@Table(name = "Review")
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class ReviewEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_id")
+    private int reviewId;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "reviewee_id")
+    private UserEntity revieweeId;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "writer_id")
+    private UserEntity writerId;
+
+    @Column(name = "rating")
+    private float rating;
+
+    @Column(name = "review_time")
+    private LocalDateTime reviewTime;
+
+    @Column(name = "review_content")
+    private String reviewContent;
+}
